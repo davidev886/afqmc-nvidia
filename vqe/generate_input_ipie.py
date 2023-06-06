0de27114bf6f50b224715ba443508aeebaccbc09
Davide Vodola <davide.vodola@basf.com>
2023-06-06 10:00:12 +0200

    Comparison between jordan wigner and jw fixed number of electrons

--- vqe/generate_input_ipie.py
+++ vqe/generate_input_ipie.py
@@ -5,10 +5,10 @@ from pyscf.scf.chkfile import dump_scf
 from openfermion.transforms import get_fermion_operator, jordan_wigner
 from openfermion.chem import MolecularData
 from openfermionpyscf import run_pyscf
-
+from openfermion import get_sparse_operator, jw_number_restrict_operator
 from src.utils_vqe import write_trial_ipie, write_hamiltonian_ipie
 from src.vqe import VqeHardwareEfficient
-
+from scipy.sparse.linalg import eigsh
 
 if __name__ == "__main__":
     np.set_printoptions(precision=6, suppress=True, linewidth=10000)
@@ -36,11 +36,32 @@ if __name__ == "__main__":
     print(f"SCF energy: {molecule.hf_energy}")
     print(f"FCI energy: {molecule.fci_energy}")
 
-    print("starting VQE")
+
     n_qubit = molecule.n_qubits
     n_electron = molecule.n_electrons
     fermionic_hamiltonian = get_fermion_operator(molecule.get_molecular_hamiltonian())
+
     jw_hamiltonian = jordan_wigner(fermionic_hamiltonian)
+    ham = get_sparse_operator(fermionic_hamiltonian, n_qubit)
+
+    energies_full_space, vectors_full_space = eigsh(ham, k=4, which='SA')
+    print(energies_full_space)
+    print("Apply jw restricted ")
+    ham_nbr_restr = jw_number_restrict_operator(get_sparse_operator(jw_hamiltonian, n_qubit), n_electron, n_qubit)
+    values_nbr_restr, vectors_nbr_restr = eigsh(ham_nbr_restr, k=4, which='SA')
+    print(values_nbr_restr)
+
+    print("full hamitlonian")
+    print(ham)
+    print(type(ham))
+    print("*" * 80)
+
+    print("restricted hamitlonian")
+    print(ham_nbr_restr)
+    print(type(ham_nbr_restr))
+    print("*" * 80)
+
+    print("starting VQE")
     vqe = VqeHardwareEfficient(n_qubits=n_qubit, n_layers=1, n_electrons=mol.nelec)
     vqe.run(jw_hamiltonian,
             init_vals=np.random.rand(vqe.num_params),
